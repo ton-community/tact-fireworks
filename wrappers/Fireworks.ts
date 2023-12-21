829f940a1ef61e75af10519309f75a385e746f92
AlexG <39581753+Reveloper@users.noreply.github.com>
2023-12-22 08:10:47 +0900

    test_update

--- wrappers/Fireworks.ts
+++ wrappers/Fireworks.ts
@@ -2,7 +2,7 @@ export * from '../build/Fireworks/tact_Fireworks';
 
 export const Opcodes = {
     set_first: 0xda1e3345,
-    launch_first: 0x6efe144b,
+    launch_first: 0xf1c5c2b7,
     launch_second: 0xa2e2c2dc,
     faked_launch: 0x39041457,
 };
